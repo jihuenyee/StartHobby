6f1cf03fec6245577336b77c0b5a6ab3324feec0
hm040723 <nghawming1@gmail.com>
2025-12-02 08:37:54 +0800

    test

--- server/index.js
+++ server/index.js
@@ -15,7 +15,11 @@ const followRoutes = require("./routes/followRoutes");
 const authRoutes = require("./routes/authRoutes");
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 app.use(express.json());
 
 app.use("/api/quizzes", quizRoutes);
